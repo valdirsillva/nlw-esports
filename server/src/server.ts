e07e817f7060d2c90a6c0efe82dcd496cc92e8b9
Valdir Silva <valdipiresba@gmail.com>
2022-09-13 00:45:24 -0300

    Iniciando frontend da aplicacao com React

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/ads', (request, response) => {
+    return response.json([
+        { id: 1, name: "Anuncio 01" },
+        { id: 2, name: "Anuncio 02" }
+    ])
+})
+
+app.listen(3333)
