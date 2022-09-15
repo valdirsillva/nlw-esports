b4672bd98c007da340a14abb9e4a51139bf1ee51
Valdir Silva <valdipiresba@gmail.com>
2022-09-15 00:04:10 -0300

    Gerando as migrates do banco de dados SQlite

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,29 @@ import express from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+    return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+    return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+    //const gameId = request.params.id
+
     return response.json([
-        { id: 1, name: "Anuncio 01" },
-        { id: 2, name: "Anuncio 02" }
+        { id: 1, name: "Anúncio 01" },
+        { id: 2, name: "Anúncio 02" },
+        { id: 3, name: "Anúncio 03" }
     ])
 })
+
+app.get('/ads/:id/discord', (request, response) => {
+    //const adId = request.params.id
+
+    return response.json([])
+})
+
 
 app.listen(3333)
